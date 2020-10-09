21212f7a6cb8383a7f0528fa230408069750351b
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2020-10-09 11:26:18 -0300

    laravel Mix dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.'], function () {
     Route::get('/', 'AuthController@showLoginForm')->name('login');
+    Route::get('home', 'AuthController@home')->name('home');
 });
