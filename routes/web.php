af41d2f2bd85bf50b385205abd3d0e8744242532
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2020-10-19 14:36:44 -0300

    Desenvolvendo recursos do usuario

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,9 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.'], fu
     Route::group(['middleware' => ['auth']], function () {
         /** Dashborad Home */
         Route::get('home', 'AuthController@home')->name('home');
+
+        Route::get('users/team', 'UserController@team')->name('users.team');
+        Route::resource('users', 'UserController');
     });
 
     /** logout */
