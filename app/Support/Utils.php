09cb96ef44bade2a3a6ae331375cbe487c342ac6
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2020-10-20 13:34:41 -0300

    tratamento de dados

---
+++ app/Support/Utils.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace LaraDev\Support;
+
+
+class Utils
+{
+    /**
+     * Limpa a string para ficar no padrão a ser salvo na base de dados
+     * @param string|null $param
+     * @return string|string[]
+     */
+    public static function clearField(?string $param)
+    {
+        if (empty($param)){
+            return '';
+        }
+
+        return  str_replace(['.', '-', '/', '(', ')', ' ' ], '', $param);
+    }
+
+    /**
+     * converte string em data para ficar no padrão a ser salvo na base de dados
+     * @param string|null $param
+     * @return string|null
+     */
+    public static function convertStringToDate(?string $param)
+    {
+        if (empty($param)){
+            return null;
+        }
+
+        list($day, $month, $year) = explode('/', $param);
+        return (new \DateTime("$day-$month-$year"))->format("Y-m-d");
+    }
+
+    /**
+     * converte string para double para ficar no padrão a ser salvo na base de dados
+     * @param string|null $param
+     * @return float|null
+     */
+    public static function convertStringToDouble(?string $param)
+    {
+        if (empty($param)){
+            return null;
+        }
+        return doubleval(str_replace(',', '.', str_replace('.', '', $param )));
+    }
+}
--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace LaraDev;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use LaraDev\Support\Utils;
 
 class User extends Authenticatable
 {
@@ -16,7 +17,44 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'last_login_at', 'last_login_ip'
+        'name',
+        'email',
+        'password',
+        'genre',
+        'document',
+        'document_secondary',
+        'document_secondary_complement',
+        'date_of_birth',
+        'place_of_birth',
+        'civil_status',
+        'cover',
+        'occupation',
+        'income',
+        'company_work',
+        'zipcode',
+        'street',
+        'number',
+        'complement',
+        'neighborhood',
+        'state',
+        'city',
+        'telephone',
+        'cell',
+        'type_of_communion',
+        'spouse_name',
+        'spouse_genre',
+        'spouse_document',
+        'spouse_document_secondary',
+        'spouse_document_secondary_complement',
+        'spouse_date_of_birth',
+        'spouse_place_of_birth',
+        'spouse_occupation',
+        'spouse_income',
+        'spouse_company_work',
+        'lessor',
+        'lessee',
+        'admin',
+        'client'
     ];
 
     /**
@@ -36,4 +74,67 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @param $value
+     */
+    public function setLessorAttribute($value)
+    {
+        $this->attributes['lessor'] = ($value === true || $value === 'on' ? 1 : 0);
+    }
+
+    public function setLesseeAttribute($value)
+    {
+        $this->attributes['lessee'] = ($value === true || $value === 'on' ? 1 : 0);
+    }
+
+    public function setDocumentAttribute($value)
+    {
+        $this->attributes['document'] = Utils::clearField($value);
+    }
+
+    public function setDateOfBirthAttribute($value)
+    {
+        $this->attributes['date_of_birth'] = Utils::convertStringToDate($value);
+    }
+
+    public function setIncomeAttribute($value)
+    {
+        $this->attributes['income'] = Utils::convertStringToDouble($value);
+    }
+
+    public function setZipcodeAttribute($value)
+    {
+        $this->attributes['zipcode'] = Utils::clearField($value);
+    }
+
+    public function setTelephoneAttribute($value)
+    {
+        $this->attributes['telephone'] = Utils::clearField($value);
+    }
+
+    public function setCellAttribute($value)
+    {
+        $this->attributes['cell'] = Utils::clearField($value);
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+
+    public function setSpouseDocumentAttribute($value)
+    {
+        $this->attributes['spouse_document'] = Utils::clearField($value);
+    }
+
+    public function setSpouseDateOfBirthAttribute($value)
+    {
+        $this->attributes['spouse_date_of_birth'] = Utils::convertStringToDate($value);
+    }
+
+    public function setSpouseIncomeAttribute($value)
+    {
+        $this->attributes['spouse_income'] = Utils::convertStringToDouble($value);
+    }
 }
